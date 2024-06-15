a437ab6932f241acb2530fe1fb052136af1d7f50
VinofSteel <v.nascimento23@yahoo.com.br>
2024-06-15 12:44:15 -0300

    feat: first commit, creating initial tokens for the lexer

---
+++ token/token.go
@@ -0,0 +1,33 @@
+package token
+
+// Constants
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	IDENT = "IDENT"
+	INT = "INT"
+
+	ASSIGN = "="
+	PLUS = "+"
+
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
+
+// Types
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
