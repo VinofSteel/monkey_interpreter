3b690f755bcf7f1322aa5d997f9f792ec3de8639
VinofSteel <v.nascimento23@yahoo.com.br>
2024-06-18 22:26:53 -0300

    feat: adding the REPL for interaction with the interpreter and creation of the console, logic to make it run

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/user"
 
+	"github.com/vinofsteel/monkeyinterpreter/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Println("Feel free to type in commands")
+	repl.Start(os.Stdin, os.Stdout)
 }
